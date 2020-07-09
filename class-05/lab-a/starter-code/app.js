6fd6a2c95168afa967e125f56361b007ae540fb6
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-09 14:07:43 +0300

    q2 done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -37,7 +37,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
- 
+    var product= a*b;
+    var returnArray=[product, "The product of "+a+" and "+b+" is "+product+"."];
+    return returnArray;
+
 
 }
 
