9ab6741f9a7adbe2339cdcfe40ee365a04455546
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-09 13:25:16 +0300

    first q done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -1,14 +1,21 @@
 'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sum() that takes in two numbers as arguments and then returns an array
+ where the first element is the sum of those numbers, and the second element is 
+ a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished,
+ uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
+
+    var sum1= a+b;
+    var returnArray=[sum1, "The sum of "+a+" and "+b+" is 11."];
+    return returnArray;
 
 }
 
